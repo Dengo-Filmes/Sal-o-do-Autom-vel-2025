cfeed5f985eed8a7ba38fe29536b6ea5fa0ca252
VictorDengo <victor@dengofilmes.com.br>
2025-11-14 11:51:46 -0300

    Update 11_50

--- Assets/Scripts/Keyboard/KeyboardController.cs
+++ Assets/Scripts/Keyboard/KeyboardController.cs
@@ -4,38 +4,45 @@ public class KeyboardController : MonoBehaviour
 {
     InputSystem_Actions action;
     Vector2 _touchPosition;
-
     bool _shouldDrag = false;
 
     [SerializeField] LeanTweenType _tweenType;
     [SerializeField] float _tweenSpeed;
 
+    Canvas parentCanvas;
+    RectTransform rectTransform;
+    RectTransform parentRect;
+
     private void Awake()
     {
         action = new();
-
-        //_touchPosition = action.UI.Point.ReadValue<Vector2>();
         action.UI.Point.performed += ctx => _touchPosition = ctx.ReadValue<Vector2>();
-    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+        rectTransform = GetComponent<RectTransform>();
+        parentRect = rectTransform.parent as RectTransform;
 
+        // pega o canvas onde ele está
+        parentCanvas = GetComponentInParent<Canvas>();
     }
 
     public void OpenCloseKeyboard(bool open)
     {
         if (_shouldDrag) return;
+
         CanvasGroup childCanvas = transform.GetChild(0).GetComponent<CanvasGroup>();
 
-        LeanTween.alphaCanvas(childCanvas, open ? 1 : 0, _tweenSpeed).setEase(_tweenType).setOnStart(() =>
-        {
-            LeanTween.moveLocalX(childCanvas.gameObject, open ? 0 : -45, _tweenSpeed).setEase(_tweenType);
-        }).setOnComplete(() =>
-        {
-            childCanvas.blocksRaycasts = open;
-        }).setDelay(0.05f);
+        LeanTween.alphaCanvas(childCanvas, open ? 1 : 0, _tweenSpeed)
+            .setEase(_tweenType)
+            .setOnStart(() =>
+            {
+                LeanTween.moveLocalX(childCanvas.gameObject, open ? 0 : -45, _tweenSpeed)
+                    .setEase(_tweenType);
+            })
+            .setOnComplete(() =>
+            {
+                childCanvas.blocksRaycasts = open;
+            })
+            .setDelay(0.05f);
     }
 
     public void DragKeyboard(bool drag)
@@ -43,11 +50,20 @@ public class KeyboardController : MonoBehaviour
         _shouldDrag = drag;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(_shouldDrag)
-            transform.position = _touchPosition;
+        if (_shouldDrag)
+        {
+            
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                parentRect,             
+                _touchPosition,         
+                parentCanvas.worldCamera,  
+                out Vector2 localPoint
+            );
+
+            rectTransform.localPosition = localPoint;
+        }
     }
 
     private void OnEnable()
